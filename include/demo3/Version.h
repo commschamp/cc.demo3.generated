4075808048fad51fe8d06dd98337e588e05a3158
Alex Robenko <arobenko@gmail.com>
2018-12-28 10:45:04 +1000

    Code generated by v1.1 of commsdsl.

---
+++ include/demo3/Version.h
@@ -0,0 +1,25 @@
+/// @file
+/// @brief Contains protocol version definition.
+
+#pragma once
+
+#include "comms/version.h"
+
+/// @brief Version of the protocol library as single numeric value
+#define DEMO3_VERSION (5U)
+
+namespace demo3
+{
+
+/// @brief Version of the protocol library as single numeric value
+inline constexpr unsigned version()
+{
+    return DEMO3_VERSION;
+}
+
+} // namespace demo3
+
+
+static_assert(COMMS_MAKE_VERSION(1, 0, 0) <= comms::version(),
+    "The version of COMMS library is too old");
+
